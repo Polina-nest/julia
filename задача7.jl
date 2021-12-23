22f177b12b2903ee80182d5fcec5f5a501faeeab
Polina-nest <46245789+Polina-nest@users.noreply.github.com>
2021-12-23 18:32:56 +0300

    Update задача7.jl

--- задача7.jl
+++ задача7.jl
@@ -1,85 +1,135 @@
-# task7.jl
-include("roblib.jl")
-#=
-    файл roblib.jl содержит определения функций:
-    move!(::Robot,::HorizonSide,::Int) или обобщенный вариант move!(::Any,::Any,::Any) - тут важно, что аргумента 3, а не 2
-    movements!(::Robot,::HorizonSide) или обобщенный вариант movements!(::Any,::Any)
-=#
+function main7!(r::Robot)
+    x, y = getStartPos(r)
 
-module ShessMark
-    import Main.inverse, Main.movements!
-    #=
-        Предполагается, что функции inverse, movements! определены в пространстве имен Main
-        (например, в результате выполнения include("roblib.jl"))
-    =#
-    import ..StartBack.move_to_start!, ..StartBack.move_to_back!
-    #=
-        Предполагается, что модуль StartBack был определен в том же пространстве имен, что и данный модуль ShessMark
-        (поэтому и ..StartBack. - эти две точки в начале означают, что модуль StartBack надо искать в пространстве имен на 1 иерархичекий
-        уровень выше данного)
-    =#
+    returnStartPos!(r, x, y)
 
-    using HorizonSideRobots
-
-    export mark_chess
-
-    FLAG_MARK = false # здесь безразлично, какое именно значение присвоено, т.к. оно потом переопределяется
-
-    function mark_chess(robot)
-        global FLAG_MARK
-        #=
-            Глобальные переменные, ввиду их особой важности, следует именовать заглаными буквами.
-            Объявление переменной как global требуется, только если ее значение ИЗМЕНЯЕТСЯ в теле функции,
-            использоваться же она может и без такого объявления
-        =#
-        num_steps = move_to_start!(robot, (Sud,West))
-        #УТВ: Робот - в юго-западном углу
-
-        FLAG_MARK = odd(sum(num_steps)) ? true : false
-        # значение FLAG_MARK определяет, нужно ли в юго-западном углу ставить маркер
-
-        #=
-            Можно было бы и не вводить локалную переменную num_steps, написав так:
-
-        FLAG_MARK = odd(sum(move_to_start!(robot, (Sud,West)))) ? true : false
-
-            или так:
-
-        FLAG_MARK = (move_to_start!(robot, (Sud,West)) |> sum |> odd) ? true : false
-
-        где |> - оператор, направляющий поток данных
-        =#
-
-        side = Ost
-        mark_chess(robot,side)
-        while isborder(Nord)==false
-            move!(robot,Nord)
-            side = inverse(side)
-            mark_chess(robot,side)
-        end
-        #УТВ: Робот - у северной границы поля И маркеры расставлены в шахматном порядке
-
-        for side in (West,Sud) movements!(robot,side) end
-        #УТВ: Робот - в юго-западном углу
-
-        move_to_back!(robot)
-        #УТВ: Робот - в исходном положении
+    cnt = 1
+    while isborder(r, Ost) == false
+        markChessSide!(r, cnt)
+        cnt += 1
+        move!(r, Ost)
     end
 
-    function mark_chess(r,side)
+    markChessSide!(r, cnt)
+
+    while cnt > 1
+        move!(r, West)
+        cnt -= 1
+    end
+
+    cnt = 0
+
+    while isborder(r, West) == false
+        move!(r, West)
+        markChessSide!(r, cnt)
+        cnt += 1
+    end
+
+    while cnt > 0
+        move!(r, Ost)
+        cnt -= 1
+    end
+end
+
+function getStartPos(r::Robot)
+    x, y = 0, 0
+
+    while isborder(r, West) == false
+        x += 1
+        move!(r, West)
+    end
+
+    while isborder(r, Sud) == false
+        y += 1
+        move!(r, Sud)
+    end
+
+    return x, y
+end
+
+function returnStartPos!(r::Robot, x, y)
+    setLeftDown!(r)
+
+    while x > 0
+        move!(r, Ost)
+        x -= 1
+    end
+
+    while y > 0
+        move!(r, Nord)
+        y -= 1
+    end
+end
+
+function getXLen(r::Robot)
+    cnt = 0
+
+    while isborder(r, Ost) == false
+        cnt += 1
+        move!(r, Ost)
+    end
+
+    while isborder(r, West) == false
+        move!(r, West)
+    end
+
+    return cnt
+end
+
+function getYLen(r::Robot)
+    cnt = 0
+
+    while isborder(r, Nord) == false
+        cnt += 1
+        move!(r, Nord)
+    end
+
+    while isborder(r, Sud) == false
+        move!(r, Sud)
+    end
+
+    return cnt
+end
+
+function setLeftDown!(r::Robot)
+    while isborder(r, Sud) == false
+        move!(r, Sud)
+    end
+    while isborder(r, West) == false
+        move!(r, West)
+    end
+end
+
+function markChessSide!(r::Robot, current)
+    if current % 2 == 1
         putmarker!(r)
-        while isborder(r,side)==false
-            move!(r,side)
+    end
+
+    cnt = 0
+
+    while isborder(r, Nord) == false
+        cnt += 1
+        move!(r, Nord)
+        if cnt % 2 + current % 2 == 1
             putmarker!(r)
         end
     end
-
-    function putmarker!(r)
-        global FLAG_MARK
-        if FLAG_MARK == true
-            HjrizonSideRobots.putmarker(r)
-        end
-        FLAG_MARK = !FLAG_MARK
+    
+    while cnt > 0
+        cnt -= 1
+        move!(r, Sud)
     end
 
+    while isborder(r, Sud) == false
+        cnt += 1
+        move!(r, Sud)
+        if cnt % 2 + current % 2 == 1
+            putmarker!(r)
+        end
+    end
+    
+    while cnt > 0
+        cnt -= 1
+        move!(r, Nord)
+    end
 end
