a92e4f8570154bc1d43edc361837fe9454390935
Polina-nest <46245789+Polina-nest@users.noreply.github.com>
2021-12-23 17:44:25 +0300

    Update задача5.jl

--- задача5.jl
+++ задача5.jl
@@ -1,32 +1,46 @@
-function mark_angles(r)
-    num_steps=[]
-    while isborder(r,Sud)==false || isborder(r,West)
-        push!(num_steps, moves!(r, West))
-        push!(num_steps, moves!(r, Sud))
-    end
+function main5!(r::Robot)
+      path = leftDown(r) #Робот в левом нижнем углу и массив path создан
 
-    for side in (Nord,Ost,Sud,West)
-        moves!(r,side) 
-        putmarker!(r)
-    end
-    
-    for (i,n) in enumerate(reverse!(num_steps))
-        side = isodd(i) ? Ost : Nord 
-        moves!(r,side,n)
-    end
+      putmarker!(r)
+      moveSide!(r, Nord)
+      putmarker!(r)
+      moveSide!(r, Ost)
+      putmarker!(r)
+      moveSide!(r, Sud)
+      putmarker!(r)
+      moveSide!(r, West)
+      #Каждый угол помечен
 
+      leftDown!(r, path) #Робот в исходном положении
+end
+
+function leftDown(r::Robot)
+      path = []
+      while isborder(r, West) == false || isborder(r, Sud) == false
+            if isborder(r, West) == false
+                  move!(r, West)
+                  push!(path, true)
+            else
+                  move!(r, Sud)
+                  push!(path, false)
+            end
+      end
+      
+      return reverse!(path)
 end
 
 
-
-function moves!(r,side)
-    num_steps=0
-    while isborder(r,side)==false
-        move!(r,side)
-        num_steps+=1
-    end
-    return num_steps
+function leftDown!(r::Robot, path)
+      for i in path
+            if i move!(r, Ost)
+            else move!(r, Nord)
+            end
+      end
 end
 
-import HorizonSideRobots.move!
-move!(r,side,num_steps)=for _ in 1:num_steps move!(r,side) end
+
+function moveSide!(r::Robot, side::HorizonSide)
+      while isborder(r, side) == false
+            move!(r, side)
+      end
+end
