ea67797b59d313632ba41430ef7fa56f6e7b2f8a
Polina-nest <46245789+Polina-nest@users.noreply.github.com>
2021-12-23 16:34:21 +0300

    Update задача1.jl

--- задача1.jl
+++ задача1.jl
@@ -1,22 +1,22 @@
-function mark_kross!(r::Robot)
-    for side in (Nord,West,Sud,Ost) # - рассматриваем все направления которые возможны
-        putmarkers!(r,side)
-        move_by_markers(r,inverse(side))
-    end
-    putmarker!(r)
-end
 
-putmarkers!(r::Robot,side::HorizonSide) = 
-    while isborder(r,side)==false 
+  
+function mark_cross!(r::Robot)
+    for side in(Nord,West,Sud,Ost)
+        putmarkers!(r,side)
+        side_inv=inverse(side)
+        move_by_marker!(r,side_inv)
+    end
+putmarker!(r)
+end
+function putmarkers!(r::Robot,side::HorizonSide)
+    while isborder(r,side)==false
         move!(r,side)
         putmarker!(r)
     end
-
-
-move_by_markers(r::Robot,side::HorizonSide) = 
-    while ismarker(r)==true 
-        move!(r,side) 
-    end # движение погка не попадет в клетку без маркера
-
-
-inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2, 4)) 
+end
+function move_by_marker!(r::Robot,side::HorizonSide)
+    while ismarker(r)==true
+        move!(r,side)
+    end
+end
+inverse(side::HorizonSide)=HorizonSide(mod(Int(side)+2,4))
