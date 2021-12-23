4275ce1d7e969eb5875fdae4fe48f4179bcaf486
Polina-nest <46245789+Polina-nest@users.noreply.github.com>
2021-12-23 18:34:54 +0300

    Update задача9.jl

--- задача9.jl
+++ задача9.jl
@@ -1,22 +1,43 @@
-function find_marker(r)
-    num_steps_max=1
-    side=Nord
-    while ismarker(r)==false
-        for _ in 1:2
-            find_marker(r,side,num_steps_max)
-            side=next(side)
+function moveSideN!(r::Robot, side::HorizonSide, n)
+    while n > 0
+        if ismarker(r) == true
+            break
         end
-        num_steps_max+=1
+        move!(r, side)
+        n -= 1
     end
 end
 
-function find_marker(r,side,num_steps_max)
-    for _ in 1:num_steps_max
-        if ismarker(r)
-            return nothing
-        end
-        move!(r,side)
-    end
-end
+function main9!(r::Robot)
+    cnt = 0
 
-next(side::HorizonSide)=HorizonSide(mod(Int(side)+1,4))
+    while true
+        if ismarker(r) == true
+            break
+        end
+
+        cnt += 1
+
+        moveSideN!(r, Ost, cnt)
+        if ismarker(r) == true
+            break
+        end
+
+        moveSideN!(r, Sud, cnt)
+        if ismarker(r) == true
+            break
+        end
+
+        cnt += 1
+
+        moveSideN!(r, West, cnt)
+        if ismarker(r) == true
+            break
+        end
+
+        moveSideN!(r, Nord, cnt)
+        if ismarker(r) == true
+            break
+        end
+    end         
+end
